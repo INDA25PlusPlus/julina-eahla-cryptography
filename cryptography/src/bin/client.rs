4b519472365b8f18d1a664f5e4e4b6cd1a5b7ccd
JulinaSavela <julina.savela@outlook.com>
2025-11-11 13:55:21 +0100

    impl decrypt_file()

--- cryptography/src/bin/client.rs
+++ cryptography/src/bin/client.rs
@@ -7,22 +7,71 @@ use aes_gcm::{
     Aes256Gcm, Nonce, Key // Or `Aes128Gcm`
 };
 use std::fs;
+use std::path::Path;
 
 
+fn build_plaintext(file_path: &str) -> std::io::Result<Vec<u8>> {
+
+    let file_content = fs::read(file_path).expect("Failed to read file");
+    let full_path = Path::new(file_path);
+    let filename = full_path.file_name()
+        .expect("Invalid file path")
+        .to_string_lossy()
+        .into_owned();
+
+    let filename_bytes = filename.as_bytes();
+
+    if filename_bytes.len() > u16::MAX as usize {
+        panic!("Filename too long");
+    }
+
+    let mut plaintext = Vec::new();
+    // Format: [ (filname 2 bytes) | (filename bytes) | file_content ]
+    plaintext.extend_from_slice(&(filename_bytes.len() as u16).to_be_bytes());
+
+    plaintext.extend_from_slice(filename_bytes);
+
+    plaintext.extend_from_slice(&file_content);
+    
+    Ok(plaintext)
+
+}
 
 fn encrypt_file(cipher: &Aes256Gcm, file_path: &str) -> Vec<u8> {
 
-    let plaintext = fs::read(file_path).expect("Failed to read file");
+    
+    let formatted_plaintext= build_plaintext(file_path).unwrap();
 
     // unique nonce for each encryption, 96 bits
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
-    let ciphertext = cipher.encrypt(&nonce, plaintext.as_ref()).expect("Encryption failed");
+    let ciphertext = cipher.encrypt(&nonce, formatted_plaintext.as_ref()).expect("Encryption failed");
 
     let mut output_vec = nonce.to_vec();
     output_vec.extend_from_slice(&ciphertext);
 
     return output_vec;
 
+}
+
+#[allow(deprecated)]
+fn decrypt_file(cipher: &Aes256Gcm, ciphertext_vec: Vec<u8>) -> (String, Vec<u8>){
+
+    // first 12 bytes is nonce
+    let (nonce_bytes, ciphertext) = ciphertext_vec.split_at(12);
+
+    
+    let nonce = Nonce::from_slice(&nonce_bytes);
+
+    let plaintext = cipher.decrypt(&nonce, ciphertext).unwrap();
+
+    let filename_len = u16::from_be_bytes([plaintext[0], plaintext[1]]) as usize;
+
+    let filename_bytes = &plaintext[2..2+filename_len];
+    let filename = String::from_utf8_lossy(filename_bytes).into_owned();
+
+    let file_content = plaintext[2+filename_len..].to_vec();
+
+    (filename, file_content)
 }
 
 
