4547c64b87e583ed0e01da91d09d72da56fe7490
Jonathan Hahne <jonathanhahne@n141-p48.eduroam.kth.se>
2025-11-12 15:43:46 +0100

    formatting changes

--- cryptography/src/bin/server.rs
+++ cryptography/src/bin/server.rs
@@ -1,21 +1,17 @@
-
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
 use serde_json;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 #[derive(serde::Deserialize, Debug)]
 struct Metadata {
     file_id: u32,
 }
 
-
-
 fn read_tcp_message(mut stream: TcpStream) -> std::io::Result<()> {
-
-    // Format på meddelande: 
+    // Format på meddelande:
     // [metadata length: 4 bytes]
-    // [metadata JSON]                 
-    // [ciphertext length: 4 bytes]    
+    // [metadata JSON]
+    // [ciphertext length: 4 bytes]
     // [ciphertext bytes]  <-- encrypted nonce + filename + file content + 16-byte tag
     let mut metadata_len_bytes = [0u8; 4]; // 4 byte buffer
     stream.read_exact(&mut metadata_len_bytes)?;
@@ -39,29 +35,21 @@ fn read_tcp_message(mut stream: TcpStream) -> std::io::Result<()> {
     let mut ciphertext_bytes = vec![0u8; ciphertext_len as usize];
     stream.read_exact(&mut ciphertext_bytes)?;
 
-
     // Spara {file_id: ciphertext_bytes} på nåt smart sätt
 
-
     Ok(())
 }
 
-fn handle_client(stream: TcpStream) -> std::io::Result<()>  {
-
-    
+fn handle_client(stream: TcpStream) -> std::io::Result<()> {
     read_tcp_message(stream)?;
 
-
     Ok(())
-
 }
 
-
 fn main() -> std::io::Result<()> {
-
     let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to listen on port --");
     println!("Listening on port --");
-  
+
     for stream in listener.incoming() {
         handle_client(stream?)?;
     }
